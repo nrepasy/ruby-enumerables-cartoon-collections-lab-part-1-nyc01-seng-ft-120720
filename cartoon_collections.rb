8d40ea6fb1d15e0040b29ef1e03f71d80e5dd4cc
nick repasy <nrepasy@gmail.com>
2020-12-03 21:30:39 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,7 +10,11 @@ end
 
 def list_dwarves(array)
   array.each_with_index {|name, index|
+<<<<<<< HEAD
   puts " #{index + 1}  #{name}"}
+=======
+  puts "index: #{index} for #{name}"
+>>>>>>> 00f0bb4cf9c42ca26bcbc99a04e4bf007cd2efcb
  
       # Use `each_with_index` to enumerate over the provided array
   #
